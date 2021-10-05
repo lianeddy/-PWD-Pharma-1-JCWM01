11cca4a5c8f153b2950fc5e470b32ef79329959f
muslimabdul8 <muslimabdul8@gmail.com>
2021-10-05 21:56:50 +0700

    mysql

--- BACKEND/database/index.js
+++ BACKEND/database/index.js
@@ -3,7 +3,7 @@ const mysql = require("mysql");
 const db = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Incorrect100%",
+  password: "Mu$lim100%",
   database: "db_pharma",
   port: 3306,
   multipleStatements: true,
