185bbad018d6883a9b4ce75c8782e100b59070f7
lianeddy <lian.eddy@gmail.com>
2021-10-08 09:26:29 +0700

    Diction fix

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,17 +7,17 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(bearerToken());
 
 app.get("/", (req, res) => {
   res.status(200).send("<h4>Integrated MySQL with express</h4>");
 });
 
-const {adminRoute, drugsRouters, userRouter} = require('./routers')
+const { adminRoute, drugsRouters, userRouter } = require("./routers");
 
 app.use("/obat", drugsRouters);
 app.use("/user", userRouter);
-app.use('/admin',adminRoute)
+app.use("/admin", adminRoute);
 
 app.listen(PORT, () => console.log("API Running: ", PORT));
--- BACKEND/routers/adminRoute.js
+++ BACKEND/routers/adminRoute.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const { adminController } = require('../controllers')
-const router = express.Router()
+const express = require("express");
+const { adminController } = require("../controllers");
+const router = express.Router();
 
-router.get('/getDataProduct', adminController.getDataProduct)
-router.get('/getDataProductId/:id', adminController.getDataProductId)
-router.post('/uploadDataProduct', adminController.uploadDataProduct)
-router.post('/editDataProduct', adminController.updateDataProduct)
-module.exports = router
+router.get("/getDataProduct", adminController.getDataProduct);
+router.get("/getDataProductId/:id", adminController.getDataProductId);
+router.post("/uploadDataProduct", adminController.uploadDataProduct);
+router.post("/editDataProduct", adminController.updateDataProduct);
+module.exports = router;
