c703ecb290066af5845e311cb7ae45f5d8f852d2
muslimabdul8 <muslimabdul8@gmail.com>
2021-10-11 09:09:42 +0700

    Update nodemailer.js

--- BACKEND/helper/nodemailer.js
+++ BACKEND/helper/nodemailer.js
@@ -4,7 +4,7 @@ const transporter = nodemailer.createTransport({
   service: "gmail",
   auth: {
     user: "shabrinaartarini46@gmail.com",
-    pass: "swzkzzfckjoxzdzv",
+    pass: "texizwpciccndlfy",
   },
   tls: {
     rejectUnauthorized: false,
