19e426a4d2caf616d9da4d9efd1f84d85f6203dd
dwikyryan <dwikyryan@gmail.com>
2021-10-26 03:23:08 +0700

    fix-served-prescription

--- FRONTEND/src/pages/Admin/Rejected.jsx
+++ FRONTEND/src/pages/Admin/Rejected.jsx
@@ -23,11 +23,13 @@ class Rejected extends React.Component {
       return (
         <tr>
           <th>{val.idrejected_prescriptions}</th>
-          <td>
+          <td className="align-middle">
             {val.nama_depan} {val.nama_belakang}
           </td>
-          <td>{moment(val.tanggal).format("DD MMMM YYYY")}</td>
-          <td>{val.status}</td>
+          <td className="align-middle">
+            {moment(val.tanggal).format("DD MMMM YYYY")}
+          </td>
+          <td className="align-middle">{val.reason}</td>
         </tr>
       );
     });
@@ -40,13 +42,13 @@ class Rejected extends React.Component {
   render() {
     return (
       <div className="justify-content-center align-items-center mx-5 my-3">
-        <table className="table table-striped text-center">
+        <table className="table table-striped table-hover text-center">
           <thead>
             <tr>
               <th scope="col">Id</th>
               <th scope="col">Applicant</th>
               <th scope="col">Rejection Date</th>
-              <th scope="col">Status</th>
+              <th scope="col">Reason</th>
             </tr>
           </thead>
           <tbody>{this.renderRejectedList()}</tbody>
